9de7c075a9f2427b3a4bf36dbfe87867cd16fb50
Teemu Tuulari <ttuulari@gmail.com>
2019-04-23 15:42:07 +0300

    Switch case to select shader program based on alphas

--- Shared/SCNText2D.swift
+++ Shared/SCNText2D.swift
@@ -33,7 +33,7 @@ public class SCNText2D {
         case centered
     }
 
-    public static func create(from string: String, withFontNamed fontName: String, fontColor: Color = float4(1.0, 1.0, 1.0, 1.0), outlineColor: Color = float4(0.0, 0.0, 0.0, 0.0), shadowColor: Color = float4(0.0, 0.0, 0.0, 0.0), smoothing: Float = 0.04, scale: SCNFloat = 1.0, lineSpacing: SCNFloat = 1.0, alignment: TextAlignment = .centered, fontWidth: Float = 0.9, outlineWidth: Float = 0.5, shadowWidth: Float = 0.5, shadowOffset: float2 = float2(0.0, 0.002)) -> SCNGeometry {
+    public static func create(from string: String, withFontNamed fontName: String, fontColor: Color = float4(1.0, 1.0, 1.0, 1.0), outlineColor: Color = float4(0.0, 0.0, 0.0, 0.0), shadowColor: Color = float4(0.0, 0.0, 0.0, 0.0), smoothing: Float = 0.04, scale: SCNFloat = 1.0, lineSpacing: SCNFloat = 1.0, alignment: TextAlignment = .centered, fontWidth: Float = 0.9, outlineWidth: Float = 0.5, shadowWidth: Float = 0.5, shadowOffset: float2 = float2(0.0, 0.0)) -> SCNGeometry {
         let jsonURL = Bundle.main.url(forResource: fontName, withExtension: "json")!
         let jsonData = try! Data(contentsOf: jsonURL)
 
@@ -53,13 +53,17 @@ public class SCNText2D {
         let shaderProgram = SCNProgram()
         shaderProgram.vertexFunctionName = "sdfTextVertex"
         
-        if outlineColor[3] == 0.0 {
-            shaderProgram.fragmentFunctionName = "sdfTextFragment"
+        switch (outlineColor[3], shadowColor[3]) {
+            case (let outline, 0.0) where outline > 0.0:
+                shaderProgram.fragmentFunctionName = "sdfTextOutlineFragment"
+
+            case (_, let shadow) where shadow > 0.0:
+                shaderProgram.fragmentFunctionName = "sdfTextOutlineShadowFragment"
+
+            default:
+                shaderProgram.fragmentFunctionName = "sdfTextFragment"
         }
-        else {
-            shaderProgram.fragmentFunctionName = "sdfTextOutlineFragment"
-        }
-        
+
         shaderProgram.isOpaque = false
         shaderProgram.library = shaderLibrary
 
